788cb0a1db8bd98569c60648a1484d783e1f38e6
Ariel Javier Felix Murillo De La Barra <119796628+ArielM457@users.noreply.github.com>
2025-07-03 14:50:00 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,8 @@ function App() {
       try {
         console.log('Todas las variables de entorno:', process.env);
         console.log('REACT_APP_API_URL específica:', process.env.REACT_APP_API_URL);
+
+        
         const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/history`, {
           headers: { 'x-user-id': userId }
         });
